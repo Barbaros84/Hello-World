6fa040505454757d2e71b0b5f9048ff7fac603a1
Mete <barbaros.dj@gmail.com>
2023-02-02 19:19:11 +0300

    Taskevich

---
+++ Example_011_Taskevich/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("insert the number: ");
+int number = int.Parse(Console.ReadLine());
+int num1 = number % 6;
+int num2 = number % 48;
+
+if (num1 == 0 && num2 == 0) Console.WriteLine("the number is a multiple of 6 and 48");
+else Console.WriteLine("number is not a multiple 6 and 48");
