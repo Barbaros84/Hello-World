d19cb00b48d95b8a72e9aff67e0a291c0cb01b61
Mete <barbaros.dj@gmail.com>
2023-02-02 17:02:41 +0300

    task

---
+++ Example_010_Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("enter a five-digit number");
+
+int number = int.Parse(Console.ReadLine());
+
+int firstDigit = number / 10000;
+int secondDigit = (number - firstDigit * 10000 ) / 10000;
+int thirdDigit = (number - firstDigit * 10000 - secondDigit * 1000) / 100;
+int fourthDigit = (number - firstDigit * 10000 - secondDigit * 1000 - thirdDigit * 100) / 10;
+int fifthDigit = number - firstDigit * 10000 - secondDigit * 1000 - thirdDigit * 100 - fourthDigit * 10;
+
+if(firstDigit == firstDigit && secondDigit == fourthDigit)
+          Console.WriteLine("you entered a palindrome!");
+else
+          Console.WriteLine("number is not a palindrome!");
+    
+
+
